217ba19cfdd71b7a6a6e7d5142b912e0102448e7
kennedyat <ktpenny8@gmail.com>
2019-04-10 14:45:43 -0400

    updates, may change

--- src/tweetText.py
+++ src/tweetText.py
@@ -1,7 +1,20 @@
 import gzip
-import openTweets
+
+import re
 
 def tweetText():
-    print(openTweets.openLine())
+        data_folder = Path(r"C:\Users\ktpenny\Desktop\Carmen-s-Code\data")
+        file_to_open = data_folder / "nusample"
 
-        #print('got line', line)
+
+        #f = open(file_to_open)
+        #print(f.read())
+        with open(file_to_open,'rt') as f:
+            for line in f:
+                #start = line.find('"text"') + 3
+                 #end = line.find('"source"', start)
+                 #print(line[start:end])
+                 m = re.search('text(.+?)source', line)
+                 if m:found = m.group(1)
+tweetText();
+#print(openTweets.openLine())
