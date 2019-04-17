b0c9e996406d26323ec80d4051ee93d8f695e952
kennedyat <ktpenny8@gmail.com>
2019-04-17 15:04:08 -0400

    Pulls out of nusample.json -Unfinished

--- src/exampleCode.py
+++ src/exampleCode.py
@@ -3,13 +3,12 @@ from pathlib import Path
 
 import json
 
-tweets = []
-dict = {}
-i = 0
-for line in open(r'C:\Users\ktpenny\Desktop\Carmen-s-Code\data\nusample.json'):
 
-    tweets.append(dict.update({json.loads(line)}))
-    print (tweets)
+with open(r'C:\Users\ktpenny\Desktop\Carmen-s-Code\data\nusample.json') as json_data:
+
+    datastore = json.load(json_data)
+    print (datastore["place"])
+
     #d = json.load(json_data)
     #print(type(d))
     #print(type(d[0]))
